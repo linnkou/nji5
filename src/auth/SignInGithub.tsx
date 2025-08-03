02dc679699582f97dfea67296e923f9eb76f8db8
linnkou <latifboudihir@gmail.com>
2025-08-04 01:33:04 +0200

    Delete src directory

--- src/auth/SignInGithub.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuthActions } from "@convex-dev/auth/react";
-
-export function SignInGithub() {
-  const { signIn } = useAuthActions();
-  return (
-    <button onClick={() => void signIn("github")}>Github</button >
-  )
-}
